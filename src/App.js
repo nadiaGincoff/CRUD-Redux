a3f3d5511891dbafa6765635fe078143d41165b2
nadiaGincoff <nadia.gincoff@gmail.com>
2020-09-18 17:28:46 -0300

    initialize store redux

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Products from './components/Products'
 import NewProduct from './components/NewProduct'
 import EditProduct from './components/EditProduct'
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store'
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className='container mt-5'>
-        <Switch>
-          <Route exact path='/' component={Products} />
-          <Route exact path='/products/new' component={NewProduct} />
-          <Route exact path='/products/edit/:id' component={EditProduct} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className='container mt-5'>
+          <Switch>
+            <Route exact path='/' component={Products} />
+            <Route exact path='/products/new' component={NewProduct} />
+            <Route exact path='/products/edit/:id' component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,1 +1,14 @@
 // each reducer has its own state
+
+const initialState = {
+    products: [],
+    error: null, 
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
