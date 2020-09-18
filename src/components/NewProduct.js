0076e81fe16c0459c2cbbae75e3fa751ee699bda
nadiaGincoff <nadia.gincoff@gmail.com>
2020-09-18 18:09:25 -0300

    define action and use useDispatch, passing information from the form to action

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+
+// Redux actions
+import { createNewProductAction } from '../actions/productActions';
 
 const NewProduct= () => {
+    // component state
+    const [name, saveName] = useState('');
+    const [price, savePrice] = useState(0);
+
+    // use useDispatch and create a function
+    const dispatch = useDispatch();
+
+    const addProduct = (product) => dispatch( createNewProductAction(product) )
+
+    const submitNewProduct = e => {
+        e.preventDefault();
+
+        // validate form 
+        if (name.trim() === '' || price <= 0) {
+            return;
+        }
+        // if there are no errors
+        
+        
+        // create the new product
+        addProduct({
+            name, 
+            price
+        })
+    }
+
     return ( 
         <div className='row justify-content-center'>
             <div className='col-md-8'>
@@ -9,7 +39,7 @@ const NewProduct= () => {
                         <h2 className='text-center mb-4 font-weight-bold'>
                             Add New Product
                         </h2>
-                        <form>
+                        <form onSubmit={submitNewProduct}>
                             <div className='form-group'>
                                 <label>Product name</label>
                                 <input 
@@ -17,13 +47,17 @@ const NewProduct= () => {
                                     className='form-control'
                                     placeholder='Product name'
                                     name='name'    
+                                    value={name}
+                                    onChange={e => saveName(e.target.value)}
                                 />
                                 <label>Product price</label>
                                 <input 
                                    type='number'
                                    className='form-control'
                                    placeholder='Product price'
-                                   name='price'        
+                                   name='price'  
+                                   value={price}
+                                   onChange={e => savePrice(Number(e.target.value))}      
                                 />                          
                             </div>
                             <button
